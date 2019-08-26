8c59cc457a5862c787ceb00aec122ce4d4135c27
Crhistian Ramirez <cramirez@four51.com>
2019-08-26 16:35:37 -0500

    ssr mostly complete
    still need to figure out something for angular-tree-component library as well as how to do this with an asp.net core server

--- src/UI/Buyer/src/app/app-routing.module.ts
+++ src/UI/Buyer/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 // components
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { FaqComponent } from './static-pages/faq/faq.component';
-// import { SupportComponent } from './static-pages/support/support.component';
+import { FaqComponent } from './static-pages/faq/faq.component';
+import { SupportComponent } from './static-pages/support/support.component';
 
 import {
   BaseResolve,
@@ -10,10 +10,10 @@ import {
   HasTokenGuard as HasToken,
 } from 'src/app/shared';
 import { HomeComponent } from 'src/app/layout/home/home.component';
-// import { TermsAndConditionsComponent } from 'src/app/static-pages/terms-and-conditions/terms-and-conditions.component';
+import { TermsAndConditionsComponent } from 'src/app/static-pages/terms-and-conditions/terms-and-conditions.component';
 import { ProductsModule } from './product/product.module';
 import { ProfileModule } from './profile/profile.module';
-// import { CheckoutModule } from './checkout/checkout.module';
+import { CheckoutModule } from './checkout/checkout.module';
 
 export function loadProductsModule() {
   return ProductsModule;
@@ -23,9 +23,9 @@ export function loadProfileModule() {
   return ProfileModule;
 }
 
-// export function loadCheckoutModule() {
-//   return CheckoutModule;
-// }
+export function loadCheckoutModule() {
+  return CheckoutModule;
+}
 
 const routes: Routes = [
   {
@@ -42,24 +42,24 @@ const routes: Routes = [
         loadChildren: './profile/profile.module#ProfileModule',
         canActivate: [isProfiledUser],
       },
-      // {
-      //   path: 'support',
-      //   component: SupportComponent,
-      // },
-      // {
-      //   path: 'faq',
-      //   component: FaqComponent,
-      // },
-      // {
-      //   path: 'terms-and-conditions',
-      //   component: TermsAndConditionsComponent,
-      // },
+      {
+        path: 'support',
+        component: SupportComponent,
+      },
+      {
+        path: 'faq',
+        component: FaqComponent,
+      },
+      {
+        path: 'terms-and-conditions',
+        component: TermsAndConditionsComponent,
+      },
       {
         path: 'products',
         loadChildren: './product/product.module#ProductsModule',
       },
-      // { path: '', loadChildren: './checkout/checkout.module#CheckoutModule' },
-      // { path: 'impersonation', redirectTo: '/home' },
+      { path: '', loadChildren: './checkout/checkout.module#CheckoutModule' },
+      { path: 'impersonation', redirectTo: '/home' },
     ],
   },
 ];
